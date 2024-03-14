c21d6c2f154c3bec4d1d1bc80356988464807974
HicaroD <hdanrlley1@gmail.com>
2024-03-14 18:10:45 -0300

    feat: considering updated server time on average calculation

--- server.py
+++ server.py
@@ -2,15 +2,19 @@ import concurrent.futures as futures
 import grpc
 import berkeley_pb2_grpc
 import berkeley_pb2
+import time
 
 
 class TimeSyncServicer(berkeley_pb2_grpc.TimeSyncServicer):
-    def __init__(self) -> None:
+    def __init__(self, server_port: int) -> None:
         super().__init__()
+        self.server_port = server_port
         self.clients = {}
 
     def SendTimeToMaster(self, request, context):
-        # Armazena o tempo do cliente no dicionário 
+        # Atualizando o tempo do servidor para utilizarmos na média
+        self.clients[self.server_port] = time.time() * 1000
+        # Atualizando o tempo do cliente para utilizarmos na média
         self.clients[request.port] = request.time
         # Pega o tempo médio dos clientes
         average_time = sum(self.clients.values()) / len(self.clients)
@@ -23,8 +27,11 @@ class TimeSyncServicer(berkeley_pb2_grpc.TimeSyncServicer):
 
 def serve():
     # Inicia o servidor na porta 50051
+    server_port = 50051
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    berkeley_pb2_grpc.add_TimeSyncServicer_to_server(TimeSyncServicer(), server)
+    berkeley_pb2_grpc.add_TimeSyncServicer_to_server(
+        TimeSyncServicer(server_port=server_port), server
+    )
     server.add_insecure_port("[::]:50051")
     print("Server is running on port 50051")
     server.start()
